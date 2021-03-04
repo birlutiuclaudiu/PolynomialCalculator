84c1657fa92f95e342dce50f48540eec273893dc
birlutiuclaudiu <Claudiu.Birlutiu@student.utcluj.ro>
2021-03-04 14:22:07 +0200

    initial commit

---
+++ src/main/java/pt2021_30226_birlutiu_claudiu_assignment_1/PolynomialCalculator.java
@@ -0,0 +1,10 @@
+package pt2021_30226_birlutiu_claudiu_assignment_1;
+
+public class PolynomialCalculator {
+
+    public static void main(String[] args){
+        View v=new View();
+        v.setVisible(true);
+
+    }
+}
